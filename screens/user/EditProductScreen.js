7072a9894e3bea9e2ac3f8084ff6354d2f2e3137
shawn barbel <barbels33@gmail.com>
2021-09-16 13:21:00 -0400

    created blank screens

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditProductScreen = () => {
+  return <div></div>;
+};
+
+export default EditProductScreen;
---
+++ screens/user/UserProductScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserProductScreen = () => {
+  return <div></div>;
+};
+
+export default UserProductScreen;
